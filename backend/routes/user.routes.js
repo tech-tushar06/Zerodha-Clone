d1ac17b1ed45d47372a1f7e9b2b2a27fe0e9718f
tech-tushar06 <tusharsharma80164@gmail.com>
2026-05-07 16:45:39 +0530

    Completed

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const { createUser, getUsers } = require("../controllers/user.controller");
+
+router.post("/", createUser);
+router.get("/", getUsers);
+
+module.exports = router;
